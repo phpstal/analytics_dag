6b1cc748c109874e8c78bf4663084837e20941e7
Vladimir Polovnikov <vrmy87@yandex.ru>
2026-02-13 21:56:47 +0300

    add

---
+++ recommendation_service/dags/sql_queries_for_intermediate_aggregation.py
@@ -0,0 +1,3 @@
+# sql_queries_for_intermediate_aggregation.py
+def sql_queries_for_intermediate_aggregation():
+    return 'sql_queries_for_intermediate_aggregation.py'
---
+++ recommendation_service/dags/sql_queries_for_metrics.py
@@ -0,0 +1,3 @@
+# sql_queries_for_metrics.py
+def sql_queries_for_metrics():
+    return 'sql_queries_for_metrics.py'
