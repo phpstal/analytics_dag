c44c8b2211d8386ded227074cdad2b7b5cf05e08
Vladimir Polovnikov <vrmy87@yandex.ru>
2026-02-13 22:41:20 +0300

    a5

--- recommendation_service/dags/calc_metrics.py
+++ recommendation_service/dags/calc_metrics.py
@@ -4,14 +4,14 @@ import sys
 from airflow.models.dag import DAG
 from airflow.decorators import task
 from sql_queries_for_intermediate_aggregation import sql_queries_for_intermediate_aggregation
-# from recommendation_service.dags.sql_queries_for_metrics import sql_queries_for_metrics
+from sql_queries_for_metrics import sql_queries_for_metrics
 
 @task
 def test_import():
     result = sql_queries_for_intermediate_aggregation()
     logging.info(result)
-    #result = sql_queries_for_metrics()
-    #logging.info(result)
+    result = sql_queries_for_metrics()
+    logging.info(result)
 
 
 with DAG(
