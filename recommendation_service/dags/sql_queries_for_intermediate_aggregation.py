9bac18cc7d406df225a94b6dd8b2820d262c250e
vrmy87 <vrmy87@yandex.ru>
2026-06-19 14:00:32 +0700

    delete file

--- recommendation_service/dags/sql_queries_for_intermediate_aggregation.py
+++
@@ -1,3 +0,0 @@
-# sql_queries_for_intermediate_aggregation.py
-def sql_queries_for_intermediate_aggregation():
-    return 'sql_queries_for_intermediate_aggregation.py'
--- recommendation_service/dags/sql_queries_for_metrics.py
+++
@@ -1,3 +0,0 @@
-# sql_queries_for_metrics.py
-def sql_queries_for_metrics():
-    return 'sql_queries_for_metrics.py'
