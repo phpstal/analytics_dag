b738ba422a1b15014fae36f775b2d3d52654237d
phpstal <45956062+phpstal@users.noreply.github.com>
2026-06-19 13:45:48 +0700

    add new file

---
+++ hello_world.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import sys
+import pymssql
+
+
+sys.path = ["/opt/airflow/dags"] + sys.path
+
+def checklib():
+    
+    print(f"pymssql version = {pymssql.__version__}  and syspath = {sys.path}")
+
+
+with DAG(dag_id="checklib",
+
+         start_date=datetime(2024,3,14),
+
+         schedule="*/5 * * * *",
+
+         catchup=False) as dag:
+
+
+    task1 = PythonOperator(
+
+            task_id="checklib",
+
+            python_callable=checklib)
+
+
+    task1
